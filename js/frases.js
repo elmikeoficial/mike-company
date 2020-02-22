3b44d73f774c1eabe3314d82dd58dff54c82abf0
elmikeoficial <61342917+elmikeoficial@users.noreply.github.com>
2020-02-22 02:21:03 -0600

    Add files via upload

---
+++ js/frases.js
@@ -0,0 +1,48 @@
+var Frase=new Array() 
+Frase[0] = "-Hola br@ :v-";
+Frase[1] = "-Las personas nomas hablan, tu sigue-";
+Frase[2] = "-pasele wuero-";
+Frase[3] = "-Los tacos son vida, recuerdalo...-";
+Frase[4] = "-sere breve, te esperan cosas hermosas-";
+Frase[5] = "-Un viaje largo se empieza con un paso-";
+Frase[6] = "-La vida no se mide en minutos, se mide en momentos-";
+Frase[7] = "-Olvidalo el editor esta aburrido-";
+Frase[8] = "-Secreto 13-05-2002 Sssh-";
+Frase[9] = "-La vida tiene muchos obstaculos tu pones los limites-";
+Frase[10] = "-Y si dejas de pensarlo y simplemente los intentas???-";
+Frase[11] = "-Yo no soy  nadie para aconsejarte pero no te rindas-";
+Frase[12] = "-si tu dia es amargo sacudelo un poco, el azucar aveces esta en el fondo-";
+Frase[13] = "--";
+Frase[14] = "-Si alguien pudo hacerlo yo tambien puedo y si nadie pudo yo sere el primero-";
+Frase[15] = "-Te podra faltar dinero pero que nunca te falte actitud-";
+Frase[16] = "-Tu sigue callando bocas-";
+Frase[17] = "-Yo creo en ti, intentalo-";
+Frase[18] = "-Eres grande recuerdalo-";
+Frase[19] = "-Sueña pero no te duermas-";
+Frase[20] = "-El camino al exito es la actitud-";
+Frase[21] = "-Cuando dios cambia nuestros planes es porque algo mejor va a llegar-";
+Frase[22] = "-Podran robarte las ideas pero no el talento-";
+Frase[23] = "-Si piensas que puedes lograrlo, estas un paso mas cerca del exito-";
+Frase[24] = "-Lo que haces hoy puede mejorar todas tus mañanas-";
+Frase[25] = "-Se rien de mi porque soy diferente yo me rio de ellos porque son todos iguales-";
+Frase[26] = "-Sonrie aunque esto no sea una foto-";
+Frase[27] = "-Al igual que el tren el exito pasa cada dia depende de ti si te subes o otro lo hace-";
+Frase[28] = "-Colecciona momentos, no cosas-";
+Frase[29] = "-No odies al que te odia, Quiere a quien te quiera-";
+Frase[30] = "-Este es tu mejor año-";
+Frase[31] = "-Oye sigue tu sueño-";
+Frase[32] = "-Yo nunca me eh rendido no lo voy a hacer porque la gente quiera-";
+Frase[33] = "-Hello Word Like si entendiste a no verdad? solo reinicia la pagina y se quita este mensaje-";
+Frase[34] = "-DATO: -";
+Frase[35] = "-DATO: -";
+Frase[36] = "-DATO: -";
+Frase[37] = "-DATO: Enganchate antes se llamaba Engancha.T-";
+Frase[38] = "-DATO: Enganchate es chido-";
+Frase[39] = "-No se tu pero yo tengo hambre-";
+Frase[40] = "-Tu lo ves normal pero llevo mas de 50 frases de estas y ya me aburri solo reinicia la pagina y sale una frase nueva-";
+ 
+var F = Frase.length;
+var aleatorio=Math.round(Math.random()*(F-1));
+function fraseAleatoria(){
+        document.write(Frase[aleatorio]);
+ }
